d942068bc59b1258dd26d468bbf39ffb465f93a9
Abhishek Sahu <abhisheksahu504@gmail.com>
2024-05-18 23:23:44 +0530

    suggestion page some work

--- src/pages/Suggestion.jsx
+++ src/pages/Suggestion.jsx
@@ -3,11 +3,18 @@ import React from "react";
 export const Suggestion = () => {
   return (
     <>
-      <div className="container">
+      <div className="container suggestion-head">
         <h1 className="super-logo">SuperApp</h1>
-        <div className="container">
-          <h1>Entertainment according to your choice</h1>
-        </div>
+        <img
+          src="/userLogo.png"
+          width={60}
+          height={60}
+          className="userLogo"
+          alt="Logo of user"
+        />
+      </div>
+      <div className="container">
+        <h1>Entertainment according to your choice</h1>
       </div>
     </>
   );
